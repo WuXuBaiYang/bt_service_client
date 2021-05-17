2ff2acc882d127d7d1dc71eaa2d3860e112cf512
jtech <wuxubaiyang@gmail.com>
2021-05-17 13:07:26 +0800

    +1

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,19 +1,8 @@
-import 'dart:convert';
-
-import 'package:bt_service_manager/clients/aria2/apis/aria2_api.dart';
-import 'package:bt_service_manager/clients/qbittorrent/apis/qb_api.dart';
 import 'package:bt_service_manager/clients/transmission/model/response.dart';
 import 'package:bt_service_manager/clients/transmission/widgets/tm_settings.dart';
-import 'package:bt_service_manager/manage/database/database_manage.dart';
-import 'package:bt_service_manager/model/base_model.dart';
-import 'package:bt_service_manager/model/server_config/aria2_config_model.dart';
-import 'package:bt_service_manager/model/server_config/qb_config_model.dart';
-import 'package:bt_service_manager/net/api.dart';
 import 'package:bt_service_manager/widgets/settings.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:hive/hive.dart';
 
 /*
 * 首页
